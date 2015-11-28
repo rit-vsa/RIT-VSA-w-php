a6231732a419cb08f3d743f26d2e45adff75a2f9
David Thong Nguyen <thnguyen2808@gmail.com>
2015-11-27 19:20:24 -0800

    index file is working and properly indented

--- index.php
+++ index.php
@@ -4,43 +4,45 @@
 <head>
   <?php
   $pageTitle = "Home";
-  include("include/head.php");
+  include("inc/head.php");
   ?>
 </head>
     
 <body>
   <?php
-  include("include/header.php");
-  require_once("include/images.php");
+  include("inc/header.php");
+  require_once("inc/images.php");
+  $images = getImagesinFolder($_SERVER["DOCUMENT_ROOT"]."/RIT-VSA/img");
   ?>
+    
   <!-- image gallery of all our photos -->
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-         <div class="carousel slide" data-ride="carousel" id="slider">
-           <div class="carousel-inner">
-             <?php
-             for($i = 0;$i < count($names);$i++){
-               if($i == 0){
-                 echo "<div class='item active'><img src='images/".$names[$i]."'></div>";
-               }
-               else{
-                 echo "<div class='item'><img src='images/".$names[$i]."'></div>";
-               }
-             }
-             ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <div class="carousel slide" data-ride="carousel" id="slider">
+          <div class="carousel-inner">
+            <?php
+              for($i = 0;$i < count($images);$i++){
+                if($i == 0){
+                  echo "<div class='item active'><img src='img/".$images[$i]."'></div>";
+                }
+                else{
+                  echo "<div class='item'><img src='img/".$images[$i]."'></div>";
+                }
+              }
+            ?>
             <!-- Next and Previous slide buttons -->
-           <a href="#slider" data-slide="prev" class="left carousel-control">
-             <span class="glyphicon glyphicon-chevron-left"></span>
-           </a>
-           <a href="#slider" data-slide="next" class="right carousel-control">
-             <span class="glyphicon glyphicon-chevron-right"></span>
-           </a>
-         </div>
-       </div>
-     </div>
-   </div>
- </div>
+            <a href="#slider" data-slide="prev" class="left carousel-control">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a href="#slider" data-slide="next" class="right carousel-control">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
     
 </html>
