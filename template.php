02cfd258f97b1fa291057c4f6d397501c2032d2f
David Thong Nguyen <thnguyen2808@gmail.com>
2015-11-28 22:56:10 -0800

    Refactoring (SEPARATION OF CONCERN). Not everything is working.
    - Moved folders around
    - css, js (and in the future, img) folders are put in assets folder
    based on categories: custom (self-made), bootstrap (anything provided by
    the framework), and other (any other 3rd party files that are too
    complicated to sort by ourselves)
    - changed inc folder to be called include
    - differentiate head.php from header.php (header bar will probly be
    called topnav, but is a debate)
    - removed footer.php, since it's not actually a web footer (which might
    be called a bottomnav)
    - made a template.php file which almost all php files should look like

---
+++ template.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php
+  $pageTitle = "...";
+  include("include/head.php");
+  ?>
+</head>      
+    
+<body>
+  <?php include("include/header.php") ?>
+  Content of the document......
+</body>
+
+</html>
